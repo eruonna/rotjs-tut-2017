4cb991ace31b81a8507197c2afc85c18f905050e
eruonna <mjkorson@gmail.com>
2017-06-20 19:19:58 -0700

    Test if the library has loaded before trying to use it

--- Game.js
+++ Game.js
@@ -1,25 +1,27 @@
 var Game = function(div) {
   this.div = div;
-  this.initialized = false;
-
-  if (!ROT.isSupported()) {
-    this.div.innerHTML = "Rot.js is not supported in your browser :-(";
-    return;
-  }
 
   this.div.innerHTML = "Rot.js is supported in your browser :-D";
-  this.initialized = true;
 };
 
 Game.init = function() {
   var div = document.getElementById("game");
+
+  if (!("ROT" in window)) {
+    div.innerHTML = "Failed to load rot.js :-(";
+    return;
+  }
+
+  if (!ROT.isSupported()) {
+    div.innerHTML = "Rot.js is not supported in your browser :-(";
+    return;
+  }
+
   var theGame = new Game(div);
 
   theGame.run();
 };
 
 Game.prototype.run = function() {
-  if (!this.initialized) return;
-
   window.alert("Game running");
 };
