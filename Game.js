72bc06d4ee972137dc34c1b76a25f1dcf702eb28
eruonna <mjkorson@gmail.com>
2017-06-26 19:46:25 -0700

    small rearchitecture: Game is no longer instantiated

--- Game.js
+++ Game.js
@@ -1,52 +1,50 @@
-var Game = function(div) {
-  this._div = div;
-  this._div.innerHTML = '';
 
-  this._display = new Game.Display(this._div);
-
-  this._player = { x: 80, y: 25 };
-};
+Game = {}
 
 Game.init = function() {
-  var div = document.getElementById("game");
+  Game._div = document.getElementById("game");
 
   if (!("ROT" in window)) {
-    div.innerHTML = "Failed to load rot.js :-(";
+    Game._div.innerHTML = "Failed to load rot.js :-(";
     return;
   }
 
   if (!ROT.isSupported()) {
-    div.innerHTML = "Rot.js is not supported in your browser :-(";
+    Game._div.innerHTML = "Rot.js is not supported in your browser :-(";
     return;
   }
 
-  var theGame = new Game(div);
+  Game._div.innerHTML = "";
 
-  theGame.run();
+  Game._display = new Game.Display(Game._div);
+
+  Game._player = { x: 80, y: 25 };
+
+  Game.run();
 };
 
-Game.prototype.run = function() {
-  window.addEventListener('keydown', this.handleEvent.bind(this));
-  this.render();
+Game.run = function() {
+  window.addEventListener('keydown', Game.handleEvent);
+  Game.render();
 };
 
-Game.prototype.render = function() {
-  this._display.clear();
-  this._display.draw(this._player.x, this._player.y, '@');
+Game.render = function() {
+  Game._display.clear();
+  Game._display.draw(Game._player.x, Game._player.y, '@');
 }
 
-Game.prototype.update = function() {
+Game.update = function() {
 }
 
-Game.prototype.handleEvent = function (ev) {
+Game.handleEvent = function (ev) {
   switch (ev.type) {
     case 'keydown':
       var cmd = Game.Keys.getKeyCommand(ev.keyCode);
       if (cmd !== false) {
         ev.preventDefault();
-        cmd.run(this._player);
-        this.update();
-        this.render();
+        cmd.run(Game._player);
+        Game.update();
+        Game.render();
       }
       break;
   }
